01b90c21acd8bc30db8f2f75f65566eeaa777501
sofiapearson <S.A.Pearson-20@student.lboro.ac.uk>
2023-11-24 07:52:17 +0000

    Update main_mfcc_test.m

--- RXD/main_mfcc_test.m
+++ RXD/main_mfcc_test.m
@@ -1,9 +1,8 @@
 clear;
 close all;
-set(gcf,'WindowStyle','docked');
 
 %data = feature_vector(audiofile, number_of_vectors, frame_duration);
-[data, fs] = audioread('tapping.mp3');
+[data, fs] = audioread('random.mp3');
 data = data(:, 1);
 % Define frame size and overlap in samples
 frame_duration = 10*1e-3;
@@ -14,12 +13,11 @@ frame_overlap = round(5*1e-3 * fs); % For a 15 ms overlap
 
 average_vector = mean(coeffs, 1);
 covariance_matrix = cov(coeffs);
-invCovarianceMatrix = inv(covariance_matrix);
 anomaly_vector = zeros(1, size(coeffs, 2));
 
 for i = 1:size(coeffs(:,1))
     diff_vector = coeffs(i, :) - average_vector;
-    anomaly_vector(i) = sqrt(diff_vector * invCovarianceMatrix * diff_vector');
+    anomaly_vector(i) = sqrt((diff_vector / covariance_matrix )* diff_vector');
 end
 number_of_frames = size(coeffs(:,1));
 length_of_sample = number_of_frames*frame_duration;
@@ -34,7 +32,7 @@ xlabel('frame number')
 ylabel('MFCC')
 colorbar;
 title('MFCC')
-caxis([-15 15]);
+clim([-15 15]);
 
 %plot thresholded data 
 thresholded_data = get_threshold(anomaly_vector, time_array);

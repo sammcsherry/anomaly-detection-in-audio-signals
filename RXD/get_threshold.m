b7b90e85cdc0486f06a97d2ee1617ffb2715c783
sofiapearson <S.A.Pearson-20@student.lboro.ac.uk>
2023-11-24 07:51:20 +0000

    Update get_threshold.m

--- RXD/get_threshold.m
+++ RXD/get_threshold.m
@@ -2,9 +2,8 @@ function thresholded_data = get_threshold(anomaly_vector, time_array)
 
    col_anomaly_vector = anomaly_vector';
    pd = fitdist(col_anomaly_vector, 'Normal');
-   s = std(pd)
-   
-   col_anomaly_vector(col_anomaly_vector<5*s) = 1;
+   s = std(pd);   
+   col_anomaly_vector(col_anomaly_vector<4*s) = 0;
    thresholded_data = col_anomaly_vector;
    
    figure,

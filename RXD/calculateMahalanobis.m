45ae21acdea3c2fc7f124265309efe8c0aa5bf22
sammcsherry <102149963+sammcsherry@users.noreply.github.com>
2023-11-27 10:56:18 +0000

    removed wrapper function

--- RXD/calculateMahalanobis.m
+++ RXD/calculateMahalanobis.m
@@ -1,3 +1,9 @@
-function RXD = calculateMahalanobis(vector, covariance_matrix, averageVector)
-    RXD = sqrt(abs(((vector - averageVector)/covariance_matrix)*(vector - averageVector).' ));
+function anomalyVector = calculateMahalanobis(coeffs)
+    averageVector = mean(coeffs, 1);
+    covarianceMatrix = cov(coeffs);
+    % Subtract the mean vector from every observation
+    delta = coeffs - averageVector;
+    % Calculate the Mahalanobis squared distance for all observations
+    anomalyVector = sqrt(sum((delta/covarianceMatrix) .* delta, 2)).';
+
 end
--- RXD/main_mfcc_test.m
+++ RXD/main_mfcc_test.m
@@ -8,19 +8,18 @@ close all;
 %% USER DEFINED INPUTS:
 audiofile = 'jar.mp3';          %add test to check input string is of correct format
 frameOverlapPercentage = 0.6;   %add test to check this is defined as a decimal between 0<= x < 1
-frameDuration = 250e-3;         %in seconds
+frameDuration = 25e-3;         %in seconds
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 [audioData,sampleRate, frameLength, frameOverlapLength, frameOverlapDuration] = extract_audio_data(audiofile,frameOverlapPercentage, frameDuration);
 
 %mel Spectorgram
-[coeffs, ~, ~] = melSpectrogram(audioData, sampleRate, 'WindowLength', frameLength, 'OverlapLength', frameOverlapLength);
-coeffs = coeffs'; % melSpectrogram does coloums as frames so must be transposed.
+%[coeffs, ~, ~] = melSpectrogram(audioData, sampleRate, 'WindowLength', frameLength, 'OverlapLength', frameOverlapLength);
+%coeffs = coeffs'; % melSpectrogram does coloums as frames so must be transposed.
 
 %mfcc
-%[coeffs, delta, deltaDelta, loc] = mfcc(audioData, sampleRate, 'WindowLength', frameLength, 'OverlapLength', frameOverlapLength);
-anomalyVector = RXDWrapperFunc(coeffs);
-
+[coeffs, delta, deltaDelta, loc] = mfcc(audioData, sampleRate, 'WindowLength', frameLength, 'OverlapLength', frameOverlapLength);
+anomalyVector = calculateMahalanobis(coeffs);
 numberOfFrames = size(coeffs,1);
 timeArray = getTimeArray(numberOfFrames, frameDuration, frameOverlapDuration);
 

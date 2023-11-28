2c65a0b7d2f7595ca4bef5b8a94130910fd14de2
sofiapearson <S.A.Pearson-20@student.lboro.ac.uk>
2023-11-28 10:19:34 +0000

    removing unused files

--- RXD/RXDWrapperFunc.m
+++
@@ -1,8 +0,0 @@
-function [anomalyVector] = RXDWrapperFunc(coeffs)
-    averageVector = mean(coeffs, 1);
-    covariance_matrix = cov(coeffs);
-    %inverseCovarianceMatrix = inv(covariance_matrix); %commented out as
-    %not needed s
-    %anomalyVector = calculateAllMahalanobis(coeffs, averageVector, inverseCovarianceMatrix);
-    anomalyVector = calculateAllMahalanobis(coeffs, averageVector, covariance_matrix);
-end

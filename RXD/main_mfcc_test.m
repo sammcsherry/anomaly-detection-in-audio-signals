c74c6c4736cd2903ccb1477bd2611a2448d94d75
sofiapearson <S.A.Pearson-20@student.lboro.ac.uk>
2023-11-29 08:51:02 +0000

    different anomaly vectors for each domain

--- RXD/main_mfcc_test.m
+++ RXD/main_mfcc_test.m
@@ -5,7 +5,7 @@ clear;
 close all;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% USER DEFINED INPUTS:
+% USER DEFINED INPUTS:
 audiofile = 'AudioFiles/jar.mp3';          %add test to check input string is of correct format
 frameOverlapPercentage = 0.6;   %add test to check this is defined as a decimal between 0<= x < 1
 frameDuration = 25e-3;         %in seconds
@@ -14,35 +14,44 @@ frameDuration = 25e-3;         %in seconds
 [audioData,sampleRate, frameLength, frameOverlapLength, frameOverlapDuration] = extract_audio_data(audiofile,frameOverlapPercentage, frameDuration);
 
 windowFunction = hamming(frameLength); % Hamming window
-bufferedSignal = buffer(audioData, frameLength, frameOverlapLength, 'nodelay');
-windowedFrames = bsxfun(@times, bufferedSignal, windowFunction);
+bufferedSignal = buffer(audioData, frameLength); %, frameOverlapLength, 'nodelay');
+
+%windowedFrames = bsxfun(@times, bufferedSignal, windowFunction);
 
 %fft NOT WORKING YET
-%{
-coeffs = fft(windowedFrames, [], 1);
-coeffs = coeffs.';
-coeffs(end, :) = [];
-coeffs = double(coeffs.^2);
-%isa(coeffs,'double')
-%coeffs = real(coeffs);
-%}
+
+coeffsFFT = fft(windowedFrames, [], 1);
+coeffsFFT = coeffsFFT.';
+coeffsFFT(end, :) = [];
+coeffsFFT = double(coeffsFFT.^2);
+%isa(coeffsFFT,'double')
+%coeffs = real(coeffsFFT);
 
 
 %mel Spectorgram
-%[coeffs, ~, ~] = melSpectrogram(audioData, sampleRate, 'WindowLength', frameLength, 'OverlapLength', frameOverlapLength);
-%coeffs = coeffs'; % melSpectrogram does coloums as frames so must be transposed.
+[coeffsMEL, ~, ~] = melSpectrogram(audioData, sampleRate, 'WindowLength', frameLength, 'OverlapLength', frameOverlapLength);
+coeffsMEL = coeffsMEL'; % melSpectrogram does coloums as frames so must be transposed.
 
 %mfcc
-[coeffs, delta, deltaDelta, loc] = mfcc(audioData, sampleRate, 'WindowLength', frameLength, 'OverlapLength', frameOverlapLength);
-anomalyVector = calculateMahalanobis(coeffs);
+[coeffsMFCC, delta, deltaDelta, loc] = mfcc(audioData, sampleRate, 'WindowLength', frameLength, 'OverlapLength', frameOverlapLength);
+
+anomalyVectorFFT = calculateMahalanobis(coeffsFFT);
+anomalyVectorMEL = calculateMahalanobis(coeffsMEL);
+anomalyVectorMFCC = calculateMahalanobis(coeffsMFCC);
+
 numberOfFrames = size(coeffs,1);
 timeArray = getTimeArray(numberOfFrames, frameDuration, frameOverlapDuration);
 
-plotAnomalyScores(timeArray, anomalyVector, coeffs)
-
+plotAnomalyScores(timeArray, anomalyVectorFFT, coeffsFFT)
+plotAnomalyScores(timeArray, anomalyVectorMEL, coeffsMEL)
+plotAnomalyScores(timeArray, anomalyVectorMFCC, coeffsMFCC)
 %plot thresholded data 
-[thresholdedData,s] = get_threshold(anomalyVector, timeArray);
+[thresholdedDataFFT,sFFT] = get_threshold(anomalyVector, timeArray);
+[thresholdedDataMEL,sMEL] = get_threshold(anomalyVector, timeArray);
+[thresholdedDataMFCC,sMFCC] = get_threshold(anomalyVector, timeArray);
 
 %remove noise from detected anomalies
 N = 10; %left and right cells to average
-cleanedAnomalies = cleanAnomalies(thresholdedData, timeArray,s, N);
+cleanedAnomaliesFFT = cleanAnomalies(thresholdedDataFFT, timeArray,sFFT, N);
+cleanedAnomaliesMEL = cleanAnomalies(thresholdedDataMEL, timeArrayMEL,sMEL, N);
+cleanedAnomaliesMFCC = cleanAnomalies(thresholdedDataMFCC, timeArrayMFCC,sMEL, N);

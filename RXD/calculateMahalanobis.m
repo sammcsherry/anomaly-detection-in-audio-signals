4cf44ccab696a9a607056a987785c41a4bf50e97
sofiapearson <S.A.Pearson-20@student.lboro.ac.uk>
2023-12-01 12:40:38 +0000

    code from group meeting
    town team doing some coding !

--- RXD/calculateMahalanobis.m
+++ RXD/calculateMahalanobis.m
@@ -1,6 +1,6 @@
 function anomalyVector = calculateMahalanobis(coeffs)
     averageVector = mean(coeffs, 1);
-    epsilon = 0e-2;  % Small constant
+    epsilon = 1e-6;  % Small constant
     covarianceMatrix = cov(coeffs) + epsilon * eye(size(coeffs, 2));
     delta = coeffs - averageVector;
     anomalyVector = sqrt(sum((delta*pinv(covarianceMatrix)) .* delta, 2)).';
--- RXD/get_threshold.m
+++ RXD/get_threshold.m
@@ -3,7 +3,7 @@ function [thresholded_data,s] = get_threshold(anomaly_vector, timeArray)
    col_anomaly_vector = anomaly_vector';
    pd = fitdist(col_anomaly_vector, 'Normal');
    s = std(pd);   
-   col_anomaly_vector(col_anomaly_vector<5*s) = 0;
+   col_anomaly_vector(col_anomaly_vector<4*s) = 0;
    thresholded_data = col_anomaly_vector;
    
    figure,

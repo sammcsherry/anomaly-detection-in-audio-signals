5f8d519f47e99aeb24d74b7721cb8552a32648ef
AdamJager <102496837+AdamJager@users.noreply.github.com>
2023-12-06 12:32:58 +0000

    Delete RXD/get_average_vector.m

--- RXD/get_average_vector.m
+++
@@ -1,4 +0,0 @@
-function average_vector = get_average_vector(matrix, number_of_vectors)
-    length_of_vector = length(matrix(1, :));
-    average_vector = (1/number_of_vectors)*sum(matrix(:,1:length_of_vector));
-end

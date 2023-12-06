bc74cc0489762e8151871851fae7a884b202498b
sofiapearson <S.A.Pearson-20@student.lboro.ac.uk>
2023-12-06 15:17:56 +0000

    clean round 2

---
+++ RXD/cleanAnomaliesX2.m
@@ -0,0 +1,9 @@
+%clean round 3
+function cleanedX2 = cleanAnomaliesX2(cleanAnomalies)
+    MaxVal = max(cleanAnomalies);
+    MinVal = min(cleanAnomalies);
+    thresh = 0.5*(MaxVal-MinVal);
+    cleanAnomalies(cleanAnomalies<thresh) = 0;
+    cleanedX2 = cleanAnomalies;
+
+end

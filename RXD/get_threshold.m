a2196fd44d715384fd0a04f869718c142583ddca
sofiapearson <S.A.Pearson-20@student.lboro.ac.uk>
2023-12-06 17:01:52 +0000

    delete

--- RXD/get_threshold.m
+++
@@ -1,8 +0,0 @@
-function [thresholded_data,s] = get_threshold(anomaly_vector)
-
-   col_anomaly_vector = anomaly_vector';
-   pd = fitdist(col_anomaly_vector, 'Normal');
-   s = std(pd);   
-   col_anomaly_vector(col_anomaly_vector<4*s) = 0;
-   thresholded_data = col_anomaly_vector;
-end

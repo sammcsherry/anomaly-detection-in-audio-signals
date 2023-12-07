94e7f63dd27a13c38e875430a2379f92791929eb
sofiapearson <S.A.Pearson-20@student.lboro.ac.uk>
2023-12-07 11:33:23 +0000

    indents

--- RXD/fftXRD.m
+++ RXD/fftXRD.m
@@ -1,8 +1,8 @@
 function [anomalyVectorFFTnorm] = fftXRD(audioData, frameLength, frameOverlapLength, N)
-% performs the steps of xrd with fft processing
-coeffsFFT = calculateFFT(audioData, frameLength, frameOverlapLength);
-anomalyVectorFFT = calculateMahalanobis(coeffsFFT);
-anomalyVectorFFTnorm = normalize(anomalyVectorFFT, 'range');
-[thresholdedDataFFT,q10, q90] = getThreshold(anomalyVectorFFT);
-cleanedAnomaliesFFT = cleanAnomalies(thresholdedDataFFT, q10, q90, N);
+    % performs the steps of xrd with fft processing
+    coeffsFFT = calculateFFT(audioData, frameLength, frameOverlapLength);
+    anomalyVectorFFT = calculateMahalanobis(coeffsFFT);
+    anomalyVectorFFTnorm = normalize(anomalyVectorFFT, 'range');
+    [thresholdedDataFFT,q10, q90] = getThreshold(anomalyVectorFFT);
+    cleanedAnomaliesFFT = cleanAnomalies(thresholdedDataFFT, q10, q90, N);
 end

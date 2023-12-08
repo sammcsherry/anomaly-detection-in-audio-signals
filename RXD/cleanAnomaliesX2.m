3c818e9227dd10449e0f65491fd8b391e0dc96c4
sofiapearson <S.A.Pearson-20@student.lboro.ac.uk>
2023-12-08 10:39:43 +0000

    (with Lux) removing useless func

--- RXD/cleanAnomaliesX2.m
+++
@@ -1,9 +0,0 @@
-%clean round 2
-function cleanedX2 = cleanAnomaliesX2(cleanAnomalies)
-    MaxVal = max(cleanAnomalies);
-    MinVal = min(cleanAnomalies);
-    thresh = 0.5*(MaxVal-MinVal);
-    cleanAnomalies(cleanAnomalies<thresh) = 0;
-    cleanedX2 = cleanAnomalies;
-
-end

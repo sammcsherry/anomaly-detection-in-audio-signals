51c14092b6938fb1edf3e189d9f5af1397e373b9
sofiapearson <S.A.Pearson-20@student.lboro.ac.uk>
2024-02-13 13:07:53 +0000

    Almost completed testPCAsnr.m
    Need to delete line 12 and uncomment line 11 when new fftXRD.m function is complete

--- RXD/testPCAsnr.m
+++ RXD/testPCAsnr.m
@@ -3,14 +3,23 @@ function testPCAsnr(audioData, frameLength, frameOverlapLength)
     varianceThreshold = (50:100).'; %change this range first 
     npts = length(varianceThreshold);
     SNR = zeros(size(varianceThreshold)); % in decibels
-    
+
     for  i = 1:npts 
-       % [anomalyVectorFFTnorm] = fftXRD(audioData, frameLength, frameOverlapLength, varianceThreshold(i));
-       [anomalyVectorFFTnorm] = fftXRDTESTTTTT(audioData, frameLength, frameOverlapLength, varianceThreshold(i));
-       SNR(i) = snr(anomalyVectorFFTnorm);
+       varThreshold = varianceThreshold(i);
+       %not sure why passing "varianceThreshold(i)" direnctly into the function doesnt work but it is what it is
+      
+       % [anomalyVectorFFTnorm] = fftXRD(audioData, frameLength, frameOverlapLength, varThreshold);
+        [anomalyVectorFFTnorm] = fftXRDTESTTTTT(audioData, frameLength, frameOverlapLength, varThreshold);
+       
+       %this SNR is relative to a sinusoidal carrier: 
+       SNR(i) = snr(anomalyVectorFFTnorm); % given in dBc (decibels relative to the carrier)
     end
+
     [pks,locs] = findpeaks(SNR);
-    figure, plot(varianceThreshold, SNR, '-o'), hold on, plot(locs, pks, '*', 'Color', 'red');
+    figure, plot(varianceThreshold, SNR, '-o'), hold on;
+    title('SNR against Variance Threshold'),
+    xlabel('Variance Threshold [%]'), ylabel('SNR [dBc]'), % dBc (decibels relative to the carrier)
+    hold on, plot((varianceThreshold(1) + locs), pks, '*', 'Color', 'red');
+    hold off;
     
 end
-        

c3284e904c7a7c098e27760f6814a1a4c4da47d4
ls-kt <102150131+ls-kt@users.noreply.github.com>
2024-02-18 20:41:01 +0000

    zeroPadding 1st draft
    This is an attempt at a zero padding function. Does not work yet, as it needs to be adjusted so that it can be incorporated into the rest of the code.

---
+++ RXD/zeroPadding.m
@@ -0,0 +1,77 @@
+function zeroPadding (audioData, frameOverlapLength, frameLength, sampleRate, varargin)
+VecA =  tempFFT
+VecB = tempMel
+VecC = tempMFCC
+
+%differenceidx = ~ismember(VecB,VecA)
+if (length(VecA) < length(VecB))&(length(VecB)< length(VecC))
+    C = length(VecC) - length(VecA)
+PaddingVecA = [zeros(1,C) VecA]
+
+    D = length(VecC) - length(VecB)
+PaddingVecB = [zeros(1,D) VecB]
+
+elseif (length(VecA) < length(VecC))&(length(VecC) < length(VecB))
+    C = length(VecB) - length(VecA)
+PaddingVecA = [zeros(1,C) VecA]  
+
+    D = length(VecC) - length(VecB)
+PaddingVecB = [zeros(1,D) VecB]
+
+elseif (length(VecC) < length(VecA))&(length(VecA) < length(VecB))
+    C = length(VecB) - length(VecC)
+PaddingVecC = [zeros(1,C) VecC]  
+
+    D = length(VecB) - length(VecA)
+PaddingVecA = [zeros(1,D) VecA]
+
+elseif (length(VecC) < length(VecB))&(length(VecB) < length(VecA))
+    C = length(VecA) - length(VecC)
+PaddingVecC = [zeros(1,C) VecC]  
+
+    D = length(VecA) - length(VecB)
+PaddingVecB = [zeros(1,D) VecB]
+
+elseif (length(VecB) < length(VecA))&(length(VecA) < length(VecC))
+    C = length(VecC) - length(VecB)
+PaddingVecB = [zeros(1,C) VecB]  
+
+    D = length(VecC) - length(VecA)
+PaddingVecA = [zeros(1,D) VecA]
+
+elseif (length(VecB) < length(VecC))&(length(VecC) < length(VecA))
+    C = length(VecA) - length(VecB)
+PaddingVecB = [zeros(1,C) VecB]  
+
+    D = length(VecA) - length(VecC)
+PaddingVecC = [zeros(1,D) VecC]
+
+elseif (length(VecB) < length(VecC))&(length(VecC) == length(VecA))
+    C = length(VecC) - length(VecB)
+PaddingVecB = [zeros(1,C) VecB]
+
+elseif (length(VecB) > length(VecC))&(length(VecC) == length(VecA))
+    C = length(VecB) - length(VecC)
+PaddingVecC = [zeros(1,C) VecC]  
+PaddingVecA = [zeros(1,C) VecA]
+
+elseif (length(VecA) < length(VecC))&(length(VecC) == length(VecB))
+    C = length(VecC) - length(VecA)
+PaddingVecA = [zeros(1,C) VecA]  
+
+elseif (length(VecA) > length(VecC))&(length(VecC) == length(VecB))
+    C = length(VecA) - length(VecC)
+PaddingVecC = [zeros(1,C) VecC]  
+PaddingVecB = [zeros(1,C) VecB]
+
+elseif (length(VecC) < length(VecA))&(length(VecA) == length(VecB))
+    C = length(VecA) - length(VecC)
+PaddingVecC = [zeros(1,C) VecC] 
+
+elseif (length(VecC) > length(VecA))&(length(VecA) == length(VecB))
+    C = length(VecC) - length(VecA)
+PaddingVecA = [zeros(1,C) VecA]  
+PaddingVecB = [zeros(1,C) VecB]
+end
+
+end

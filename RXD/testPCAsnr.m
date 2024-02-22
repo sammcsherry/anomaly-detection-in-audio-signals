831e892d0745d9956d19aaabbd93932d1c279646
sofiapearson <S.A.Pearson-20@student.lboro.ac.uk>
2024-02-22 14:55:26 +0000

    updated func to call from audio class
    Function generalised to all audio files as it now calls the anomaly values from the class based on the choice of input data.

--- RXD/testPCAsnr.m
+++ RXD/testPCAsnr.m
@@ -1,28 +1,41 @@
-function testPCAsnr(audioData, frameLength, frameOverlapLength, sampleRate)
-    
+%{
+audiofile = 'AudioFiles/jar.mp3'; %add test to check input string is of correct format
+frameOverlapPercentage = 0.6;  %decimal
+frameDuration = 25e-4;         %seconds
+
+[audioData, sampleRate, frameLength, frameOverlapLength, frameOverlapDuration] = extractAudioData(audiofile,frameOverlapPercentage, frameDuration);
+
+%remove silence at start of audio file:
+[audioData, startingDataPoint] = removeSilence(audioData);
+audioFile = 'tapping.mp3';
+%}
+
+function testPCAsnr(audioFile, audioData, frameLength, frameOverlapLength, sampleRate)
+
     varianceThreshold = (50:100).'; %change this range if desired
     nPoints = length(varianceThreshold);
     SNR = zeros(size(varianceThreshold)); % in dBc - decibels relative to carrier
     PSNR = zeros(size(varianceThreshold)); % in dB - decibels
 
-    %for PSNR acquisition method: (only works for 'tapping.mp3')
-
-    refSignal = findAnomalyReference(audioData, [40, 44.2],[0.5, 0.5], frameLength, sampleRate, frameOverlapLength);
+    %for PSNR acquisition method:    
+    audioClass = AudioFiles();
+    anomalyTimes = audioClass.getFileData(audioFile).AnomalyCentreTimes;
+    anomalyRanges = audioClass.getFileData(audioFile).AnomalyDuration;
+    refSignal = findAnomalyReference(audioData, anomalyTimes, anomalyRanges, frameLength, sampleRate, frameOverlapLength);
  
     for  i = 1:nPoints 
-       [anomalyVectorFFTnorm] = fftXRD(audioData, frameLength, frameOverlapLength, varianceThreshold(i));
+       [anomalyVectorFFTnorm] = fftRXD(audioData, frameLength, frameOverlapLength, varianceThreshold(i));
        
        %this SNR is relative to a sinusoidal carrier: 
        SNR(i) = snr(anomalyVectorFFTnorm); % given in dBc (decibels relative to the carrier)
 
-       %the PSNR is calculated relative to a synthetic reference audio signal, which I made based on anomaly instances given by Thales:  
-       %% PSNR IS ONLY FOR "TAPPING" 
+       %the PSNR is calculated relative to a synthetic reference audio signal, made using anomaly instances given by Thales:  
        PSNR(i) = psnr(anomalyVectorFFTnorm, refSignal); 
 
     end
 
     [peaks,locations] = findpeaks(SNR);
-    figure, plot(varianceThreshold, SNR, '-o'), hold on;
+     figure, plot(varianceThreshold, SNR, '-o'), hold on;
     title('SNR against Variance Threshold'),
     xlabel('Variance Threshold [%]'), ylabel('SNR [dBc]'), % dBc (decibels relative to the carrier)
     hold on, plot((varianceThreshold(1) + locations), peaks, '*', 'Color', 'red');

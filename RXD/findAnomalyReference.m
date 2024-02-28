49c87e66d3ec7d79995ffa18ec0cb00c808b36b9
sammcsherry <102149963+sammcsherry@users.noreply.github.com>
2024-02-28 09:52:03 +0000

    Update findAnomalyReference.m

--- RXD/findAnomalyReference.m
+++ RXD/findAnomalyReference.m
@@ -1,19 +1,15 @@
 % Is this function used and what for as the team and I are confused as to
 % what folder to match this to - Cyrus \(^_^)/
-function [anomalyReference] = findAnomalyReference(audioData, anomalyTimes, anomalyRanges, frameLength, sampleRate, frameOverlapLength)
-    [numElements,~] = size(audioData);
-    totalTime = numElements/sampleRate;
-    frames = buffer(audioData, frameLength, frameOverlapLength, 'nodelay');
-    frames = frames(:, 1:end-1);
-    numFrames = size(frames, 2);
-    numAnomalies = size(anomalyTimes, 2);
+% it's used to find the ideal signal so that we can perform signal to noise
+% ratio which requires a reference signal
+function anomalyReference = findAnomalyReference(audioData, anomalyTimes, anomalyRanges, frameLength, sampleRate, numFrames)
+    totalTime = length(audioData) / sampleRate;
+    frameTime = frameLength / sampleRate;
     anomalyReference = zeros(numFrames, 1);
-    for index = 1:numAnomalies
-       anomalyPosition = anomalyTimes(index)/totalTime;
-       anomalyFrame = ceil(anomalyPosition*numFrames);
-       anomalyFrameRange = ceil((anomalyRanges(index)*sampleRate)/frameLength);
-       anomalyReference(anomalyFrame:anomalyFrame+anomalyFrameRange) = 1;
-       anomalyReference(anomalyFrame:anomalyFrame-anomalyFrameRange) = 1;
+    
+    for index = 1:length(anomalyTimes)
+       startFrame = max(1, floor((anomalyTimes(index) / totalTime) * numFrames));
+       endFrame = min(numFrames, startFrame + ceil((anomalyRanges(index) / frameTime)));
+       anomalyReference(startFrame:endFrame) = 1;
     end
-    anomalyReference = anomalyReference';
 end

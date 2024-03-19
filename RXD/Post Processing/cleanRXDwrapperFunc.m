e0a0d4e83339ae145ace3f5f53eb5d34f32d73c0
AdamJager <102496837+AdamJager@users.noreply.github.com>
2024-03-19 20:24:18 +0000

    Delete RXD/Post Processing/cleanRXDwrapperFunc.m

--- RXD/Post Processing/cleanRXDwrapperFunc.m
+++
@@ -1,5 +0,0 @@
-function finalAnomalies = cleanRXDWrapperFunc(anomalyVector, setThreshold, neighbours)
-    thresholdedData = getThreshold(anomalyVector, setThreshold);
-    cleanedAnomalies = cleanAnomalies(thresholdedData, neighbours);
-    finalAnomalies = cleanedAnomalies;
-end

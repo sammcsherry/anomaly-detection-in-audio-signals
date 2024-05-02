c11a775c3674e5d49247b475a8dd2d7df623e40f
sammcsherry <s.p.mcsherry-21@student.lboro.ac.uk>
2024-05-02 14:52:32 +0100

    Update preparebState.m

--- RXD/Pre-Processing/preparebState.m
+++ RXD/Pre-Processing/preparebState.m
@@ -1,10 +1,10 @@
 function [bState] = preparebState(audioData, frameLength,frameOverlapLength)
-    % split signal into frames
     frames = buffer(audioData, frameLength, frameOverlapLength, 'nodelay');
     frames = frames(:, 1:end-1);
-    % apply windowing
     window = hamming(frameLength);
-    windowedFrames = bsxfun(@times, frames, window);
-    bState = mean(windowedFrames, 2);
-    bState = bState ./ norm(bState);
+    frames = bsxfun(@times, frames, window);
+    test = size(frames)
+    bState = mean(frames, 2);
+    bState = bState / norm(bState);
+    bState = bState';
 end

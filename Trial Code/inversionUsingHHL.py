a0a187e3dfd16e6b9ecbaae5791281d56c776004
AdamJager <a.jager-21@student.lboro.ac.uk>
2024-05-06 20:52:26 +0100

    Testing the HHL part of Qiskit
    Monday meeting between campus team to investigate the HHL algorithm, this is the code that we used for testing by changing the matrix and number of qubits
    
    Co-Authored-By: CyrusCrawford <102588217+CyrusCrawford@users.noreply.github.com>
    Co-Authored-By: riship1 <78993888+riship1@users.noreply.github.com>

---
+++ Trial Code/inversionUsingHHL.py
@@ -0,0 +1,73 @@
+import numpy as np
+import qiskit
+from linear_solvers import NumPyLinearSolver, HHL
+from qiskit.quantum_info import Statevector
+from linear_solvers.matrices.tridiagonal_toeplitz import TridiagonalToeplitz
+from scipy.sparse import diags
+import time
+
+
+#algorithm_globals.massive=True
+qiskit.utils.algorithm_globals.massive=True
+
+""" method 1
+matrix = np.array([[1, -1/3], [-1/3, 1]])
+vector = np.array([1, 0])
+naive_hhl_solution = HHL().solve(matrix, vector)
+classical_solution = NumPyLinearSolver().solve(matrix, vector/np.linalg.norm(vector))
+
+tridi_matrix = TridiagonalToeplitz(1, 1, -1 / 3)
+tridi_solution = HHL().solve(tridi_matrix, vector)
+"""
+
+NUM_QUBITS = 2
+MATRIX_SIZE = 2 ** NUM_QUBITS
+# entries of the tridiagonal Toeplitz symmetric matrix
+# pylint: disable=invalid-name
+a = 1
+b = -1/3
+
+matrix = diags([b, a, b],
+               [-1, 0, 1],
+               shape=(MATRIX_SIZE, MATRIX_SIZE)).toarray()
+
+vector = np.array([1] + [0]*(MATRIX_SIZE - 1))
+# run the algorithms
+classical_solution = NumPyLinearSolver(
+                        ).solve(matrix, vector / np.linalg.norm(vector))
+naive_hhl_solution = HHL().solve(matrix, vector)
+
+startTime = time.time()
+tridi_matrix = TridiagonalToeplitz(NUM_QUBITS, a, b)
+tridi_solution = HHL().solve(tridi_matrix, vector)
+endTime = time.time()
+
+
+print(classical_solution)
+print(naive_hhl_solution)
+print(tridi_solution)
+
+print('classical Euclidean norm:', classical_solution.euclidean_norm)
+print('naive Euclidean norm:', naive_hhl_solution.euclidean_norm)
+print('tridiagonal Euclidean norm:', tridi_solution.euclidean_norm)
+print(f"precentage difference between naive and tridi: {naive_hhl_solution.euclidean_norm / tridi_solution.euclidean_norm}")
+
+
+naive_sv = Statevector(naive_hhl_solution.state).data
+tridi_sv = Statevector(tridi_solution.state).data
+
+# Extract vector components; 10000(bin) == 16 & 10001(bin) == 17
+naive_full_vector = np.array([naive_sv[16], naive_sv[17]])
+tridi_full_vector = np.array([tridi_sv[16], tridi_sv[17]])
+
+print('naive raw solution vector:', naive_full_vector)
+print('tridi raw solution vector:', tridi_full_vector)
+#print(naive_sv)
+#print("                        ")
+#print(tridi_sv)
+#print(naive_sv.shape)
+#print(tridi_sv.shape)
+
+
+print(f"Elapsed time for tridi_solution: {endTime - startTime} for {NUM_QUBITS} Qubits")
+

8c4c8a8331434808829eb82f70f9448379be39d0
sofiapearson <S.A.Pearson-20@student.lboro.ac.uk>
2024-05-08 14:50:30 +0100

    changing PCA input to be component number based and not variance threshold based
    Co-Authored-By: AdamJager <102496837+AdamJager@users.noreply.github.com>
    Co-Authored-By: Luxana <102150131+ls-kt@users.noreply.github.com>
    Co-Authored-By: sammcsherry <102149963+sammcsherry@users.noreply.github.com>

--- RXD/RXD/fftRXD.m
+++ RXD/RXD/fftRXD.m
@@ -1,15 +1,14 @@
 function [anomalyVectorFFTnorm] = fftRXD(audioData, frameLength, frameOverlapLength, varargin)
     if length(varargin) == 1
-        varianceThreshold = varargin{1};
+        numComponents = varargin{1};
     end
 
     coeffsFFT = calculateFFT(audioData, frameLength, frameOverlapLength);
     
     % only perform PCA if varianceThreshold is provided
     if ~isempty(varargin)
-        coeffsFFT = performPCA(coeffsFFT, varianceThreshold);
+        coeffsFFT = performPCA(coeffsFFT, numComponents);
     end
-    
     anomalyVectorFFT = calculateMahalanobis(coeffsFFT);
     anomalyVectorFFTnorm = normalize(anomalyVectorFFT, 'range');
 end
